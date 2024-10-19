e069379e1fe4f2ef24c22a84d3eae309464b3e5c
ToniXWD <807077266@qq.com>
2024-10-19 14:24:30 +0800

    trace across http and func

--- src/dice_server.rs
+++ src/dice_server.rs
@@ -1,23 +1,142 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-use opentelemetry::trace::{Span, Status, TraceError, Tracer};
+use std::collections::HashMap;
+use std::str::FromStr;
+
+use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use awc::http::header::{HeaderMap, HeaderName, HeaderValue};
+use opentelemetry::trace::{SpanKind, TraceContextExt, TraceError, Tracer};
+use opentelemetry::Context;
 use opentelemetry::{global, KeyValue};
+use opentelemetry_http::HeaderInjector;
 use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::propagation::TraceContextPropagator;
 use opentelemetry_sdk::trace as sdktrace;
 use opentelemetry_sdk::{runtime, Resource};
 use opentelemetry_semantic_conventions::resource::SERVICE_NAME;
 use rand::Rng;
 
+fn inject_context(request: &mut HeaderMap, cx: &Context) {
+    // 使用 OpenTelemetry 的 HTTP 传播器 (propagator) 注入追踪上下文到 HTTP 请求头
+
+    let mut r_headers = http::HeaderMap::new();
+
+    global::get_text_map_propagator(|propagator| {
+        propagator.inject_context(&cx, &mut HeaderInjector(&mut r_headers));
+    });
+
+    println!("randnum: r_headers: {:?}", &r_headers);
+
+    for (key, value) in r_headers.iter() {
+        let header_name = HeaderName::from_str(key.as_str()).unwrap();
+        let header_value = HeaderValue::from_str(value.to_str().unwrap()).unwrap();
+
+        request.insert(header_name, header_value);
+    }
+}
+
+fn extract_context(req: &HttpRequest) -> Context {
+    global::get_text_map_propagator(|propagator| {
+        let mut headers: HashMap<String, String> = HashMap::new();
+
+        for (key, value) in req.headers().iter() {
+            headers.insert(key.to_string(), value.to_str().unwrap().to_string());
+        }
+        propagator.extract(&headers)
+    })
+}
+
+fn get_cx_from_parent_cx<'a>(
+    tracer_name: String,
+    spam_name: String,
+    parent_cx: Option<&Context>,
+) -> Context {
+    let span;
+    let tracer = global::tracer(tracer_name);
+    match parent_cx {
+        Some(cx) => {
+            // 使用提取到的上下文作为父上下文，创建一个新的 span
+            span = tracer
+                .span_builder(spam_name)
+                .with_kind(SpanKind::Server)
+                .start_with_context(&tracer, cx);
+        }
+        None => {
+            span = tracer
+                .span_builder(spam_name)
+                .with_kind(SpanKind::Server)
+                .start(&tracer);
+        }
+    }
+
+    Context::current_with_span(span)
+}
+
 #[get("/randnum")]
 async fn randnum() -> impl Responder {
-    // 获取全局追踪器
-    let tracer = global::tracer("randnum");
-    // 创建一个span，用于追踪当前请求
-    let mut span = tracer.start("randnum");
-    span.set_status(Status::Ok);
+    let cx = get_cx_from_parent_cx("dice_server".to_string(), "randnum".to_string(), None);
 
-    let random_number = rand::thread_rng().gen_range(1..7);
-    println!("Generated random number: {}", random_number);
+    println!("randnum: Current context: {:?}", cx);
+    println!("randnum: Current span: {:?}", cx.span());
+
+    let mut request = awc::Client::default().get("http://127.0.0.1:8080/gen_num");
+
+    // 将注入数据 r_headers 添加到 headers 中
+    let req_headers = request.headers_mut();
+    inject_context(req_headers, &cx);
+
+    let mut response = request.send().await.unwrap();
+    let body = response.body().await.unwrap();
+
+    cx.span()
+        .add_event("Received response from gen_num", vec![]);
+
+    HttpResponse::Ok().body(body)
+}
+
+#[get("/gen_num")]
+async fn gen_num(req: HttpRequest) -> impl Responder {
+    // 使用 OpenTelemetry 的 HTTP 传播器 (propagator) 从 HTTP 请求头中提取追踪上下文
+    let parent_cx = extract_context(&req);
+
+    println!("gen_num: parent_cx: {:?}", parent_cx);
+    println!("gen_num: parent_cx.span: {:?}", parent_cx.span());
+
+    let cx = get_cx_from_parent_cx(
+        "dice_server".to_string(),
+        "gen_num".to_string(),
+        Some(&parent_cx),
+    );
+
+    let mut random_number: i32 = rand::thread_rng().gen_range(1..10);
+    random_number *= 2;
+
+    // 生成奇数 or 偶数?
+    let is_odd = is_odd(&cx);
+    if is_odd {
+        random_number += 1;
+    }
+
+    cx.span().add_event(
+        "Generated random number",
+        vec![opentelemetry::KeyValue::new(
+            "number",
+            random_number.to_string(),
+        )],
+    );
+
+    // 返回响应
     HttpResponse::Ok().body(random_number.to_string())
+}
+
+fn is_odd(cx: &Context) -> bool {
+    let cx = get_cx_from_parent_cx("dice_server".to_string(), "is_odd".to_string(), Some(cx));
+
+    // 50% 的概率返回 true，50% 的概率返回 false
+    let res = rand::thread_rng().gen_bool(0.5);
+    cx.span().add_event(
+        "odd or even",
+        vec![opentelemetry::KeyValue::new("is odd?", res.to_string())],
+    );
+    res
 }
 
 // 初始化追踪提供者 (Tracer Provider)，该函数返回一个全局的 `TracerProvider`
@@ -44,7 +163,8 @@ fn init_tracer_provider() -> Result<opentelemetry_sdk::trace::TracerProvider, Tr
 // 初始化全局追踪器，将 `TracerProvider` 设置为全局
 fn init_tracer() {
     let tracer_provider = init_tracer_provider().expect("Failed to initialize tracer provider.");
-    global::set_tracer_provider(tracer_provider.clone());
+    global::set_tracer_provider(tracer_provider);
+    global::set_text_map_propagator(TraceContextPropagator::new());
 }
 
 // 主函数，启动异步运行时
@@ -52,7 +172,7 @@ fn init_tracer() {
 async fn main() -> std::io::Result<()> {
     init_tracer();
 
-    HttpServer::new(|| App::new().service(randnum))
+    HttpServer::new(|| App::new().service(randnum).service(gen_num))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
